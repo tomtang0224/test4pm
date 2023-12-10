d36b900ddd2b8f095b0c21f4fb57cf7a9defa282
tomtang0224 <63864886+tomtang0224@users.noreply.github.com>
2023-12-10 21:42:19 +0800

    10-12-2023 9:42pm.

---
+++ admin_dashboard_header.php
@@ -0,0 +1,22 @@
+  <!-- Navigation Bar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Project Management</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="edit_group.php">Grouping</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Task Management</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
