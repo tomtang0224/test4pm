76e338bdae18bae474671175cc6f6a28cb11ea77
tomtang0224 <63864886+tomtang0224@users.noreply.github.com>
2024-01-03 21:46:06 +0800

    ok

--- teacher_dashboard_header.php
+++ teacher_dashboard_header.php
@@ -1,6 +1,6 @@
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="teacher_dashboard.php">Project Management System</a>
+    <a class="navbar-brand" href="teacher_dashboard.php">Project Management System (Teacher)</a>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
@@ -8,9 +8,11 @@
             </li>
         </ul>
         <ul class="navbar-nav">
+
             <li class="nav-item">
                 <a class="nav-link" href="">Welcome, <?php echo $_SESSION['user_email']; ?></a>
             </li>
+
             <li class="nav-item">
                 <a class="nav-link" href="logout.php">Logout</a>
             </li>
